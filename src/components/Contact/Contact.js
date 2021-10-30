36cb0513c24567da2ae15eb157ee6adeed5be34e
myb12 <mohammadyasinbappy@outlook.com>
2021-10-31 00:45:45 +0600

    Code are re-factored

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
                     <div className="col-lg-4 col-md-6 col-sm-6"><h2 className="section-title">Contact Us</h2></div>
                     <div className="col-lg-4 col-md-3 col-sm-3"><hr className="title-line" /></div>
                 </div>
-                <div method="post" className=" row contact-form">
+                <div className=" row contact-form">
                     <div className="col-lg-6 col-md-6">
                         <div id="google-map">
                             <iframe title="This is a unique title" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14607.605510543724!2d90.38426189999998!3d23.75089605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1634623431110!5m2!1sen!2sbd" width="100%" height="100%" frameBorder="0" style={{ border: 0 }} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
